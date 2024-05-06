92ed03aa4b6a3baf96471e86ba17fa89235e66cc
Your Name <you@example.com>
2024-05-06 01:27:17 -0500

    fixing

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -2,7 +2,7 @@ export default function createReportObject(employeesList) {
   const employeesObj = {
     allEmployees: { ...employeesList },
     getNumberOfDepartments(employeesList) {
-    return Object.keys(employeesList).length;
+      return Object.keys(employeesList).length;
     },
   };
   return employeesObj;
