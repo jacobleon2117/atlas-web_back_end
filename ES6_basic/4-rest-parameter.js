5368642be56e8fb9924a94fe3a2770c64e5f22b2
Your Name <you@example.com>
2024-05-06 01:23:52 -0500

    fixing using ESlint

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,7 +1,7 @@
 export default function returnHowManyArguments(...args) {
-    let total = 0;
-    for (let i = 0; i < args.length; i += 1) {
-      total += 1;
-    }
-    return total;
+  let total = 0;
+  for (let i = 0; i < args.length; i += 1) {
+    total += 1;
   }
+  return total;
+}
