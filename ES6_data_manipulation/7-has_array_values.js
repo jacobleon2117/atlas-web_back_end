9c3b86f2089ee44207ff1f612245d12d3f32a6be
Your Name <you@example.com>
2024-05-08 00:33:43 -0500

    fixing

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,7 +1,7 @@
 export default function hasValuesFromArray(set, arr) {
   for (let i = 0; i < arr.length; i += 1) {
     if (set.has(arr[i]) === false) {
-    return false;
+      return false;
     }
   }
   return true;
