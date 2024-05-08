073e6495b41fe3a0673518499f52e9b52553aa71
Your Name <you@example.com>
2024-05-08 00:43:20 -0500

    fixing

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,34 +1,34 @@
 export default class Airport {
-    constructor(name, code) {
-      this.name = name;
-      this.code = code;
-    }
-  
-    toString() {
-      return `[object ${this.code}]`;
-    }
-  
-    set name(val) {
-      if (typeof (val) === 'string') {
-        this._name = val;
-      } else {
-        throw TypeError('Name must be a string');
-      }
-    }
-  
-    get name() {
-      return this._name;
-    }
-  
-    set code(val) {
-      if (typeof (val) === 'string') {
-        this._code = val;
-      } else {
-        throw TypeError('Code must be a string');
-      }
-    }
-  
-    get code() {
-      return this._code;
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  toString() {
+    return `[object ${this.code}]`;
+  }
+
+  set name(val) {
+    if (typeof (val) === 'string') {
+      this._name = val;
+    } else {
+      throw TypeError('Name must be a string');
     }
   }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(val) {
+    if (typeof (val) === 'string') {
+      this._code = val;
+    } else {
+      throw TypeError('Code must be a string');
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+}
