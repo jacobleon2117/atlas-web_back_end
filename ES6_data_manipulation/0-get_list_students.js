74c5b5bbc8aae4edcc76b82940151853641222ca
Your Name <you@example.com>
2024-05-08 00:03:45 -0500

    adding #0

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+export default function getListStudents() {
+  const s1 = { firstName: 'Guillaume', id: 1, location: 'San Francisco' };
+  const s2 = { firstName: 'James', id: 2, location: 'Columbia' };
+  const s3 = { firstName: 'Serena', id: 5, location: 'San Francisco' };
+
+  const studentArray = [s1, s2, s3];
+
+return studentArray;
+}
