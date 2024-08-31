082070b98a4d33f1686abd6a6b5d842a9f9e6050
Your Name <you@example.com>
2024-08-31 18:55:08 -0500

    adding docs to the module level

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+"""
+This module provides a function to add two floating-point numbers.
+"""
+
+
 def add(a: float, b: float) -> float:
     """
     Add two floating-point numbers and return their sum.
