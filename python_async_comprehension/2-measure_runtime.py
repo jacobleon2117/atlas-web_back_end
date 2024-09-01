3f9925869ce5a556dc036f566e57eb34f1a140fb
Your Name <you@example.com>
2024-08-31 21:31:25 -0500

    indets for gather

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -23,9 +23,9 @@ async def measure_runtime() -> float:
     start_time = time.time()
 
     await asyncio.gather(async_comprehension(),
-                        async_comprehension(),
-                        async_comprehension(),
-                        async_comprehension())
+                         async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension())
 
     end_time = time.time()
     return end_time - start_time
