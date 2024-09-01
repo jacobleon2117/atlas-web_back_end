0fae5a37e57af031f4bfdd22e4bfac4dc021a11d
Your Name <you@example.com>
2024-08-31 21:06:06 -0500

    chnage float or int

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -8,7 +8,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay: float = 10.0) -> float:
+async def wait_random(max_delay: int = 10.0) -> float:
     """
     Asynchronous coroutine that waits for a random delay and returns it.
 
