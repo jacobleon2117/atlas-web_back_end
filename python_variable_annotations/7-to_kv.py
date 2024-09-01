aa5c7bdfe8c71f7e4e42538b95d0534c817f69bf
Your Name <you@example.com>
2024-08-31 20:49:14 -0500

    pyvaild

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -4,12 +4,14 @@
 This module provides a function to create a tuple containing a string and the square of a number.
 """
 
+
 from typing import Union, Tuple
+
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Create a tuple where the first element is a string and the second element is the square of a number.
-
+    
     Args:
         k (str): A string to be the first element of the tuple.
         v (Union[int, float]): An integer or float to be squared and used as the second element of the tuple.
