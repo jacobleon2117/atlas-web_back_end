9a367a6ccf175200a8ff43b0a120c64336a6f8d8
Your Name <you@example.com>
2024-09-11 18:24:13 -0500

    fixing WP

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -6,8 +6,8 @@ BasicCache module
 from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
-    """ BasicCache inherits from BaseCaching 
-        and implements a basic caching system 
+    """ BasicCache inherits from BaseCaching
+        and implements a basic caching system
     """
 
     def __init__(self):
@@ -15,14 +15,14 @@ class BasicCache(BaseCaching):
         Initialize BasicCache 
         """
         super().__init__()
-    
+
     def put(self, key, item):
         """ 
         Add an item to the cache
         """
         if key is not None and item is not None:
             self.cache_data[key] = item
-    
+
     def get(self, key):
         """ 
         Retrieve an item from the cache
