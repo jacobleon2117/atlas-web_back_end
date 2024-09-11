b79be7062463647de1ec988737e0ee09d93d83a6
Your Name <you@example.com>
2024-09-11 18:25:31 -0500

    fixing

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
 BasicCache module
 """
 
@@ -10,24 +10,24 @@ class BasicCache(BaseCaching):
         and implements a basic caching system
     """
 
+
     def __init__(self):
-        """ 
-        Initialize BasicCache 
+        """
+        Initialize BasicCache
         """
         super().__init__()
 
     def put(self, key, item):
-        """ 
+        """
         Add an item to the cache
         """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ 
+        """
         Retrieve an item from the cache
         """
         if key is not None:
             return self.cache_data.get(key)
         return None
-    
