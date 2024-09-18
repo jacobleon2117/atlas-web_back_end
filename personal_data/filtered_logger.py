c48c138605e936a5159826472da505be9a38b6fc
jacobleon2117 <143765559+jacobleon2117@users.noreply.github.com>
2024-09-18 16:16:40 -0500

    fixing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -15,7 +15,8 @@ def filter_datum(
     separator: str
 ) -> str:
     """
-        Redact specified fields in a log message with a given redaction string.
+        Redact specified fields in a
+        log message with a given redaction string.
     """
     pattern = '|'.join(
         [f'{field}=[^{separator}]*' for field in fields]
@@ -30,7 +31,8 @@ def filter_datum(
 
 class RedactingFormatter(logging.Formatter):
     """
-        Custom logging formatter that redacts specified fields in log messages.
+    Custom logging formatter that
+    redacts specified fields in log messages.
     """
 
     REDACTION = "***"
@@ -39,19 +41,20 @@ class RedactingFormatter(logging.Formatter):
 
     def __init__(self, fields: List[str]):
         """
-            Initialize the RedactingFormatter with the specified fields to redact.
+        Initialize the RedactingFormatter with the specified fields to redact.
         """
         super().__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """
-            Format the log record by redacting specified fields.
+        Format the log record by redacting specified fields.
         """
-        message = record.getMessage()
-        return filter_datum(
+        log = filter_datum(
             self.fields,
             self.REDACTION,
-            message,
+            record.getMessage(),
             self.SEPARATOR
         )
+        record.msg = log
+        return super().format(record)
