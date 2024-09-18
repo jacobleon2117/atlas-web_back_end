57c5042c0c4c5c0b3f8276c185197e8b29653eae
jacobleon2117 <143765559+jacobleon2117@users.noreply.github.com>
2024-09-18 17:08:12 -0500

    adding to encrypt

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+        Hashes a password using bcrypt with a generated salt.
+    """
+    salt = bcrypt.gensalt()
+
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+        Validates if the provided password matches the hashed password.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
