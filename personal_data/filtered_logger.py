8a8fba849c76204a23c59fae67c2c27cd213b007
jacobleon2117 <143765559+jacobleon2117@users.noreply.github.com>
2024-09-18 16:01:17 -0500

    fixing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,26 +3,9 @@
     Filtered Logger - module
 """
 
-import logging
 import re
 
 
 def filter_datum(fields, redaction, message, separator):
-    pattern = f'({"|".join(map(re.escape, fields))}){re.escape(separator)}'
-    return re.sub(pattern, f'{redaction}{separator}', message)
-
-class RedactingFormatter(logging.Formatter):
-    """
-        Redacting Formatter class
-    """
-
-    REDACTION = "***"
-    SEPARATOR = ";"
-
-    def __init__(self, fields):
-        super(RedactingFormatter, self).__init__()
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        message = super(RedactingFormatter, self).format(record)
-        return filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
+    pattern = '|'.join([f'{field}=[^\\{separator}]*' for field in fields])
+    return re.sub(pattern, lambda m: m.group(0).split('=')[0] + '=' + redaction, message)
