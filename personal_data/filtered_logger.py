480a4b2a499d5041372741d474a53ee1717b4560
jacobleon2117 <143765559+jacobleon2117@users.noreply.github.com>
2024-09-18 16:03:35 -0500

    fixing pycode

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,6 +7,9 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str],
+                redaction: str,
+                message: str,
+                separator: str) -> str:
     pattern = '|'.join([f'{field}=[^\\{separator}]*' for field in fields])
     return re.sub(pattern, lambda m: m.group(0).split('=')[0] + '=' + redaction, message)
