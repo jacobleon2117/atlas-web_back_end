1d9a09c001e120714274129e3dc87c8001fcbc08
jacobleon2117 <143765559+jacobleon2117@users.noreply.github.com>
2024-09-18 16:07:44 -0500

    fixing pycodestyle adding more to filter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,11 +5,36 @@
 
 import re
 from typing import List
+import logging
+
+def filter_datum(
+    fields: List[str],
+    redaction: str,
+    message: str,
+    separator: str
+) -> str:
+    pattern = '|'.join(
+        [f'{field}=[^{separator}]*' for field in fields]
+    )
+    return re.sub(
+        pattern,
+        lambda m: m.group(0).split('=')[0] + '=' + redaction,
+        message
+    )
 
 
-def filter_datum(fields: List[str],
-                redaction: str,
-                message: str,
-                separator: str) -> str:
-    pattern = '|'.join([f'{field}=[^\\{separator}]*' for field in fields])
-    return re.sub(pattern, lambda m: m.group(0).split('=')[0] + '=' + redaction, message)
+class RedactingFormatter(logging.Formatter):
+    """
+        Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        return filter_datum(self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
