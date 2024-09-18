65a48fef032a8f3464d1b6e735c66959e2d43df8
jacobleon2117 <143765559+jacobleon2117@users.noreply.github.com>
2024-09-18 16:02:13 -0500

    fixing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,8 +4,9 @@
 """
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     pattern = '|'.join([f'{field}=[^\\{separator}]*' for field in fields])
     return re.sub(pattern, lambda m: m.group(0).split('=')[0] + '=' + redaction, message)
