ac08fda44c7a5c706fdf983d2d31641f50d34206
jacobleon2117 <143765559+jacobleon2117@users.noreply.github.com>
2024-09-21 21:53:56 -0500

    adding

---
+++ Session_authentication/api/v1/app.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+"""
+    Route module for the API
+"""
+import os
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import CORS
+
+auth = None
+auth_type = os.getenv('AUTH_TYPE')
+if auth_type == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+@app.before_request
+def before_request():
+    """
+        Function executed before each request to filter incoming requests.
+
+        It checks if authentication is required for the request path.
+        If authentication is required and not provided, it aborts the request
+        with appropriate status codes:
+        - 401 Unauthorized: if the authorization header is missing.
+        - 403 Forbidden: if the current user cannot be determined.
+    """
+    if auth is None:
+        return
+    public_routes = ['/api/v1/status/',
+                     '/api/v1/unauthorized/',
+                     '/api/v1/forbidden/']
+
+    if request.path not in public_routes:
+        if auth.require_auth(request.path, public_routes):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            request.current_user = auth.current_user(request)  # Assign current user
+            if request.current_user is None:
+                abort(403)
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """
+        Handler for 404 Not Found errors.
+
+        Returns a JSON response indicating that the requested resource was not found.
+    """
+    return jsonify({"error": "Not found"}), 404
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """
+        Handler for 401 Unauthorized errors.
+
+        Returns a JSON response indicating that the request requires user authentication.
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """
+        Handler for 403 Forbidden errors.
+
+        Returns a JSON response indicating that the server understands the request,
+        but refuses to authorize it.
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
---
+++ Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""
+    authentication - module
+"""
+from flask import request
+from typing import List, TypeVar, Optional
+
+User = TypeVar('User')
+
+
+class Auth:
+    """
+    Base class for managing authentication in the API.
+
+    This class provides methods to determine if authentication is
+    required for specific paths, retrieve the authorization header,
+    and obtain the current user based on the request.
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Determines if authentication is required for the given path.
+
+        Args:
+            path (str): The requested API endpoint.
+            excluded_paths (List[str]): List of endpoints that
+            do not require authentication.
+
+        Returns:
+            bool: True if authentication is required, False otherwise.
+        """
+        if path is None:
+            return True
+        if not excluded_paths:
+            return True
+
+        path = path.rstrip('/')
+        for excluded_path in excluded_paths:
+            excluded_path = excluded_path.rstrip('/')
+            if path == excluded_path:
+                return False
+
+        return True
+
+    def authorization_header(self, request=None) -> Optional[str]:
+        """
+        Retrieves the authorization header from the request.
+
+        Args:
+            request: The incoming request object.
+
+        Returns:
+            Optional[str]: The value of the 'Authorization'
+            header, or None if it does not exist.
+        """
+        if request is None:
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> Optional[User]:
+        """
+        Retrieves the current user from the request.
+
+        Args:
+            request: The incoming request object.
+
+        Returns:
+            Optional[User]: An instance of the user if
+            authenticated, or None if not.
+        """
+        return None
