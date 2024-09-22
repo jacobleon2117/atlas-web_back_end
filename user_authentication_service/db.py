f6295edf3b417e57aca47436e9166a19ae640244
jacobleon2117 <143765559+jacobleon2117@users.noreply.github.com>
2024-09-21 22:44:02 -0500

    fxing

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,29 +1,34 @@
 #!/usr/bin/env python3
-"""
-DB module
+"""DB module
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from user import User, Base
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
+from typing import TypeVar
+
+from user import Base, User
 
 
 class DB:
-    """DB class
+    """
+        DB class
     """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+            DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
+        """
+            session
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -32,16 +37,11 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """
-        Add a new user to the database with the provided email and hashed password.
-        
-        Args:
-            email (str): The user's email address.
-            hashed_password (str): The hashed password for the user.
-        
-        Returns:
-            User: The newly created User object.
+            new user
         """
+
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
+        self._session.refresh(new_user)
         return new_user
