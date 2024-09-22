57e397efd9fd99fa6266fe6cd901f2ea5988210e
jacobleon2117 <143765559+jacobleon2117@users.noreply.github.com>
2024-09-21 22:03:39 -0500

    fixing

--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
-""" Module of Users views
+""" 
+    User - module
 """
 from api.v1.views import app_views
+from api.v1.auth.basic_auth import BasicAuth
 from flask import abort, jsonify, request
 from models.user import User
+from typing import TypeVar
+
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def view_all_users() -> str:
@@ -14,16 +18,25 @@ def view_all_users() -> str:
     all_users = [user.to_json() for user in User.all()]
     return jsonify(all_users)
 
-@app_views.route('/users/me', methods=['GET'], strict_slashes=False)
-def view_current_user() -> str:
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def view_current_user(user_id: str = None) -> str:
     """ GET /api/v1/users/me
     Return:
       - Current authenticated User object JSON represented
       - 404 if the User ID doesn't exist or no user is authenticated
     """
-    if request.current_user is None:
+    if user_id is None:
         abort(404)
-    return jsonify(request.current_user.to_json())
+    if user_id == 'me':
+        if request.current_user is None:
+            return None
+        else:
+            return jsonify(request.current_user.to_json())
+    user = User.get(user_id)
+    if user is None:
+        abort(404)
+    return jsonify(user.to_json())
+
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def view_one_user(user_id: str = None) -> str:
@@ -32,17 +45,20 @@ def view_one_user(user_id: str = None) -> str:
       - User ID
     Return:
       - User object JSON represented
-      - 404 if the User ID doesn't exist or is 'me' and not authenticated
+      - 404 if the User ID doesn't exist
     """
-    if user_id == "me":
-        return view_current_user()  # Use the new method for "me"
-
     if user_id is None:
         abort(404)
+    if user_id == 'me':
+        if request.current_user is None:
+            return None
+        else:
+            return jsonify(request.current_user.to_json())
     user = User.get(user_id)
     if user is None:
         abort(404)
     return jsonify(user.to_json())
+
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id: str = None) -> str:
