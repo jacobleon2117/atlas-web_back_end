b4079f40539721ceb3d69eb9d3ff7d46591f9a48
jacobleon2117 <143765559+jacobleon2117@users.noreply.github.com>
2024-09-21 22:41:46 -0500

    fixing

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -15,8 +15,8 @@ class User(Base):
 
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(String(255), nullable=False, unique=True)
-    hashed_password = Column(String(255), nullable=False)
-    session_id = Column(String(255), nullable=True)
-    reset_token = Column(String(255), nullable=True)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
