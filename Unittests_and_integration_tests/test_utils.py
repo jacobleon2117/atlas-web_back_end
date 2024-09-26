a45bf69417d117692ff1cc6b08160b7baf847b76
jacobleon2117 <143765559+jacobleon2117@users.noreply.github.com>
2024-09-25 23:33:31 -0500

    adding to test_access

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -10,8 +10,11 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+    def test_access_nested_map_exception(self, nested_map, path, expected_exception_key):
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), f"'{expected_exception_key}'")
+
 
 if __name__ == "__main__":
     unittest.main()
