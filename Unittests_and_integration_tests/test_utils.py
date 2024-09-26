40021a0c20d8f3506130d094cf3826c25bbc5401
jacobleon2117 <143765559+jacobleon2117@users.noreply.github.com>
2024-09-25 23:39:55 -0500

    adding testjson

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import get_json, access_nested_map
+
 
 class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
@@ -21,6 +23,24 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
         self.assertEqual(str(context.exception), f"'{expected_exception_key}'")
+
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch("utils.requests.get")
+    def test_get_json(self, test_url, test_payload, mock_get):
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        result = get_json(test_url)
+
+        mock_get.assert_called_once_with(test_url)
+
+        self.assertEqual(result, test_payload)
+
 
 if __name__ == "__main__":
     unittest.main()
