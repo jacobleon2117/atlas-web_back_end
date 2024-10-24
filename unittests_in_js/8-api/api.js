132b4b8fa4fb425b63630526088d15581bec19ae
jacobleon2117 <jacobleon2117@gmail.com>
2024-10-24 16:41:26 -0500

    fixing

--- unittests_in_js/8-api/api.js
+++ unittests_in_js/8-api/api.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const app = express();
+const port = 7865;
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the payment system');
+    res.status(200).send('Welcome to the payment system');
 });
 
-module.exports = app;
-
-if (require.main === module) {
-    const PORT = 7865;
-    app.listen(PORT, () => {
-        console.log(`API available on localhost port ${PORT}`);
-    });
-}
+app.listen(port, () => {
+    console.log('API available on localhost port 7865');
+})
--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,25 +1,11 @@
-const request = require('supertest');
-const app = require('./api');
-const { expect } = require('chai');
+const axios = require('axios');
+const { expect, assert } = require('chai');
 
-describe('Index page', () => {
-    let server;
-
-    before((done) => {
-        server = app.listen(7865, done);
-    });
-
-    after((done) => {
-        server.close(done);
-    });
-
-    it('should return a status code of 200', async () => {
-        const res = await request(server).get('/');
-        expect(res.status).to.equal(200);
-    });
-
-    it('should return the correct result', async () => {
-        const res = await request(server).get('/');
-        expect(res.text).to.equal('Welcome to the payment system');
+describe('API index page integration test', () => {
+    it('should return a successful status code and the landing message', () => {
+        axios.get('localhost:7865/').then((res) => {
+            assert.equal(res.status, 200);
+            assert.equal(res.data, 'Welcome to the payment system');
+        });
     });
 });
