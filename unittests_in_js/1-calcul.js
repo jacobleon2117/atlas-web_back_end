75b56d01b095a5431c2e451880f40d8ecfe95629
jacobleon2117 <jacobleon2117@gmail.com>
2024-10-24 14:15:28 -0500

    fixing

--- unittests_in_js/1-calcul.js
+++ unittests_in_js/1-calcul.js
@@ -1,20 +1,18 @@
-const calculateNumber = (type, a, b) => {
-    const roundedA = Math.round(a); // Round a to the nearest integer
-    const roundedB = Math.round(b); // Round b to the nearest integer
-
+function calculateNumber(type, a, b) {
+    const roundA = Math.round(a);
+    const roundB = Math.round(b);
     switch (type) {
-        case 'SUM':
-            return roundedA + roundedB; // Return the sum of roundedA and roundedB
-        case 'SUBTRACT':
-            return roundedA - roundedB; // Return the difference
-        case 'DIVIDE':
-            if (roundedB === 0) {
-                return 'Error'; // Handle division by zero
-            }
-            return Math.round(roundedA / roundedB); // Return the rounded result of the division
-        default:
-            throw new Error('Invalid operation type'); // Handle invalid operation type
+      case 'SUM':
+        return roundA + roundB;
+      case 'SUBTRACT':
+        return roundA - roundB;
+      case 'DIVIDE':
+        if (roundB === 0){
+          return 'Error';
+        }
+        return roundA / roundB;
     }
-};
-
-module.exports = calculateNumber;
+  
+  }
+    
+    module.exports = calculateNumber;
